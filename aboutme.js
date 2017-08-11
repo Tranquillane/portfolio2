b32a7aaa3b5d668209e94dae1dd4e98c21c82aaa
Tranquillane <Tranquillane@users.noreply.github.com>
2017-08-11 09:51:51 -0700

    Add files via upload

---
+++ aboutme.js
@@ -0,0 +1,3 @@
+function aboutMe(){
+  document.getElementById("demo").innerHTML = "I am UnusualNapkin, an aspiring programming student. I like video games, reading, and writing. I especially like role playing (video) games.";
+}
